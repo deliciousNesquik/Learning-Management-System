3fd4f465c9b0080e7c615df0c4d0348a352d9a11
deliciousNesquik <byte.in4matic@gmail.com>
2026-01-28 17:50:52 +0500

    Теперь компонент фильтра регистрируется и сам знает какое у него имя и передает его дальше

---
+++ Components/Shared/CardsView/FiltersContext.cs
@@ -0,0 +1,22 @@
+﻿namespace LMS.Components.Shared.CardsView;
+
+public sealed class FiltersContext
+{
+    public Dictionary<string, string> DisplayNames { get; }
+    public HashSet<string> RegisteredFilters { get; } = new();
+
+    public FiltersContext(Dictionary<string, string> displayNames)
+    {
+        DisplayNames = displayNames;
+    }
+
+    public void Register(string filterKey)
+    {
+        RegisteredFilters.Add(filterKey);
+    }
+
+    public string GetDisplayName(string filterKey)
+        => DisplayNames.TryGetValue(filterKey, out var name)
+            ? name
+            : filterKey;
+}
