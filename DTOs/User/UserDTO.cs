fde845b3030d4e382e1eec3229bbf6ca326c29ce
deliciousNesquik <byte.in4matic@gmail.com>
2026-01-30 16:58:00 +0500

    Промежуточный коммит. Новая система управления пользователями

---
+++ DTOs/User/UserDTO.cs
@@ -0,0 +1,28 @@
+﻿namespace LMS.DTOs.User;
+
+public class UserDto
+{
+    public Guid Uuid { get; init; }
+    public string Login { get; init; } = "";
+    
+    public required string Surname { get; set; }
+    public required string Name { get; set; }
+    public string? Patronymic { get; set; }
+    
+    public DateTime CreatedAt { get; init; }
+    public Guid CreatedBy { get; init; }
+    
+    public bool IsActive { get; init; }
+    
+    
+    /// <summary>
+    /// Поле типа jsonb содержащее дополнительную информацию о пользователе
+    /// в формате (json). Пример использования данного поля:
+    /// {
+    ///     post: "Пожарник",
+    ///     affiliation: "ООО 'Олимп'",
+    ///     insurance : "5346573252",
+    /// }
+    /// </summary>
+    public JsonContent? AdditionalInfo { get; init; }
+}
